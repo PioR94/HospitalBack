e51a7ede849d8368602e7f19db49670c221629c4
PioR <piotr_r@tutanota.com>
2023-09-25 20:45:56 +0200

    add specialization

---
+++ routers/specialization.ts
@@ -0,0 +1,15 @@
+import {Router} from "express";
+import {SpecializationRecord} from "../records/specialization.record";
+
+export const specializationRouter = Router();
+
+specializationRouter
+
+.get('/', async (req, res) => {
+        const response = await SpecializationRecord.getAll();
+
+        const specializations = response.map(r => (r.specialization)
+        )
+
+        res.json(specializations);
+})
